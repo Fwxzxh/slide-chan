05c7a3f6c314a9d32e5c22e244174b8dd7d4551d
fwxzxh <jdemeca01@gmail.com>
2026-01-27 03:51:02 -0600

    Initial Commit

---
+++ slide-chan/slide_chanApp.swift
@@ -0,0 +1,17 @@
+//
+//  slide_chanApp.swift
+//  slide-chan
+//
+//  Created by Jorge Mejia on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct slide_chanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
